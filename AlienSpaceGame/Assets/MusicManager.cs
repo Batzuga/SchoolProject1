deb452790e01b4ff459961feedbc1a56985f62a7
Batzuga <jere.ahvenus@gmail.com>
2017-10-31 16:44:28 +0200

    Added Unity Project
    Unity Project first

---
+++ AlienSpaceGame/Assets/MusicManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+	void Awake ()
+    {
+        audioSource = GameObject.Find("ScriptBlock").GetComponent<AudioSource>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
